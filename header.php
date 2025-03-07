cd688873be2d1694ae4d40d2b0285da80a6a10ff
dido739 <152557154+dido739@users.noreply.github.com>
2025-03-07 08:41:07 +0200

    Update header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Реддит Клонинг</title>
+    <title>Clonix</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
